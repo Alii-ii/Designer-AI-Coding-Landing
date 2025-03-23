ea51fa4325c2f748b93d9e7b8fcd51b5de6c946f
Alii <772984045@qq.com>
2025-03-24 00:18:57 +0800

    feat: 更新页脚组件，添加新的标语和感谢信息，优化样式和动画效果

--- src/components/screens/Footer.jsx
+++ src/components/screens/Footer.jsx
@@ -3,19 +3,45 @@ import footerImage from '@/assets/images/footer/footer.svg';
 export const Footer = () => {
   return (
     <footer className="w-full flex flex-col items-center justify-center mt-[100px] overflow-x-hidden overflow-y-hidden">
-      <div className="text-center mb-8">
-        <div className="text-[60px] leading-[80px] tracking-[0.1em] text-[#3E3E3E] font-['JetBrains_Mono'] font-normal shine-text">
-          Design for a better life
-        </div>
-        <div className="text-[60px] leading-[80px] tracking-[0.1em] text-[#3E3E3E] font-['JetBrains_Mono'] font-normal shine-text">
+      
+      {/* Slogen */}
+      <div className="text-[60px] leading-[80px] tracking-[0.1em] font-['JetBrains_Mono'] font-normal text-center mb-8">
+        <div className={`
+          bg-[length:200%_100%]
+          bg-gradient-to-r from-[#3E3E3E] via-[#FFFFFF] to-[#3E3E3E]
+          inline-block
+          text-transparent
+          bg-clip-text
+          animate-[shine_6s_linear_infinite]
+        `}>
+          Design for better life
+          <br />
           with ai coding
         </div>
+        <style jsx>{`
+          @keyframes shine {
+            0% { background-position: 200% 0; }
+            100% { background-position: -200% 0; }
+          }
+        `}</style>
       </div>
-      <img 
-        src={footerImage}
-        alt="Footer"
-        className="w-[1440px] object-contain"
-      />
+
+
+      <div className="relative w-full flex flex-col items-center gap-1 mt-8 mb-24">
+        {/* 分割线 */}
+        <div className="w-[100vw] h-[1px] bg-gradient-to-r from-transparent via-white/30 to-transparent" />
+        
+        {/* 第一行文字 */}
+        <div className="text-white/60 text-[14px] font-['PingFang_SC'] font-normal leading-[20px] text-center mt-4">
+          核心本地商业/美团平台/平台产品部/设计部
+        </div>
+
+        {/* 第二行文字 */}
+        <div className="text-center text-white/30 text-xs font-normal leading-[20px] font-['PingFang_SC']">
+          感谢基础研发平台 Nocode 项目组、美团 App HRBP 团队、美团平台学习发展团队对本活动的大力支持
+        </div>
+      </div>
+      
     </footer>
   );
 }; 
