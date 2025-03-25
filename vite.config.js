68636cd83df07cf3851376e95e95f725019a9d30
huangyili06 <huangyili06@meituan.com>
2025-03-25 13:16:18 +0800

    ci: 优化部署配置

--- vite.config.js
+++ vite.config.js
@@ -11,5 +11,10 @@ export default defineConfig({
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url))
     }
+  },
+  server: {
+    port: 8082,
+    host: true,
+    open: true
   }
 });
